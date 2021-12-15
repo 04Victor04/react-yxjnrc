e2218fe6ce92ef137081034baf48f36a96fa372f
04Victor04 <victorsernaabascal444@gmail.com>
2021-12-15 14:07:01 +0100

    añadido css, checkbox

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import './style.css';
-import {Etiqueta1} from './components/Etiqueta1';
 
+import { Lista } from './components/Lista';
 
 export default function App() {
-  let listaElemento=["Elemento1","Elemento2","Elemento3"];
-  listaElemento1.push("Elemento1");
+  const elementos1 = ['Elemento (Comp) 1', 'Elemento (Comp) 2'];
+  const elementos2 = [
+    'Elemento (Comp) 3',
+    'Elemento (Comp) 4',
+    'Elemento (Comp) 5',
+  ];
+  const elementos3 = ['Elemento (Comp) 6'];
+
   return (
     <div>
-      <h1>PrimerComponente</h1>
-      <Etiqueta1 titulo="Comp1" nombre="pepe" icono=":(" elementos("listaElemento1")/>
-      <Etiqueta1 titulo="Comp2" nombre=":)" icono=":)"/>
-      <Etiqueta1 titulo="Comp3" nombre=":(" icono=":("/>
-      <Etiqueta1 titulo="Comp3" nombre="1"icono=":)"/>
-      <h2>SegundoComponente</h2>
-     
+      <h1>Hola (desde componente)</h1>
+      <Lista titulo="Componente 1" icono=":(" elementos={elementos1} />
+      <Lista titulo="Componente 2" icono=":*" elementos={elementos2} />
+      <Lista titulo="Componente 3" icono=";)" elementos={elementos3} />
     </div>
   );
 }
