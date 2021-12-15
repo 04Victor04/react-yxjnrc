f92c7a8e8987f6e6c2666ccf04957e342d25bccb
04Victor04 <victorsernaabascal444@gmail.com>
2021-12-15 12:45:04 +0100

    cambios

--- src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Etiqueta1 } from './Etiqueta1';
-export function List(props) {
-  
-  return (<div>
-    {props.titulo} - {props.nombre}
-    <ul>{elementos}</ul>
-  </div>);
-}
