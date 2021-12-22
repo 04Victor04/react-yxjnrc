e813184e3c4d74fc787b31560f6a1a1938d98eae
04Victor04 <victorsernaabascal444@gmail.com>
2021-12-22 13:36:09 +0100

    clase lista correcta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
-import { Lista } from './components/Lista';
-import  ListaClase  from './components/ListaClase';
+import Lista from './components/Lista';
+import ListaClase from './components/ListaClase';
+
 export function App() {
   const elementos1 = [
-    { done: false, texto: 'Elemento (Comp 1)', prioridad: 'alta' },
-    { done: true, texto: 'Elemento (Comp 2)', prioridad: 'media' },
+    { done: false, texto: 'Hacer la compra', prioridad: 'alta' },
+    { done: true, texto: 'Sacar al perro', prioridad: 'media' },
   ];
   const elementos2 = [
-    { done: false, texto: 'Elemento (Comp 3)' },
-    { done: true, texto: 'Elemento (Comp 4)' },
-    { done: false, texto: 'Elemento (Comp 5)' },
+    { done: false, texto: 'Tomates' },
+    { done: true, texto: 'Queso' },
+    { done: false, texto: 'Leche' },
   ];
-  const elementos3 = [{ done: false, texto: 'Elemento (Comp 6)' }];
+  const elementos3 = [{ done: false, texto: 'Estudiar React.js' }];
 
   return (
     <div>
       <h1>Listas de tareas</h1>
-      <Lista titulo="Tareas de la casa" icono="♥" elementos={elementos1} />
-      <ListaClase titulo="Lista de la compra" icono="✌" elementos={elementos2} />
-      <Lista titulo="ToDos del curso" icono="🙌" elementos={elementos3} />
+      <Lista
+        titulo="Tareas de la casa (funciones)"
+        icono="♥"
+        elementos={elementos1}
+      />
+      <ListaClase
+        titulo="ToDos del curso (clases)"
+        icono="🙌"
+        elementos={elementos3}
+      />
     </div>
   );
 }
